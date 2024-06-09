3978ef3077a4b294b2428334ebc1fedb6c932e93
Ahmed Bibi <hola@b1b1.es>
2024-06-10 01:33:45 +0200

    Line edited on vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/signature-generator/' : '',
+  publicPath: process.env.NODE_ENV === 'production' 
+  ? '/signature-generator/' 
+  : '/',
   outputDir: 'signature-generator',
   assetsDir: '',
   productionSourceMap: false
